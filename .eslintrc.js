57b23d8a86a77a17f500ccf36f2a4895f7bb4a48
Shunya078 <kikukan007@gmail.com>
2021-11-16 19:16:01 +0900

    [add] some package

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  env: {
+    es6: true,
+    node: true,
+    browser: true,
+  },
+  settings: {
+    'svelte3/typescript': require('typescript'),
+  },
+  plugins: ['svelte3', '@typescript-eslint'],
+  overrides: [
+    {
+      files: ['**/*.svelte'],
+      processor: 'svelte3/svelte3',
+    },
+  ],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    // for svelte
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      { prefer: 'type-imports' },
+    ],
+  },
+}
